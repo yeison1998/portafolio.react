f6ebe42f19a4dfb67aeff4cebd49979ffbbeb3fd
yeison pareja <ypareja1998@gmail.com>
2019-07-17 12:31:32 -0500

    actualizacion

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/*const About = function(){
+    return <h1>Hola mundo geek</h1>
+}
+//Funcion anonima
+const AboutDos = () => {
+    return <h1>Hola mundo geek</h1> 
+}*/
+//Funcion flecha
+
+const About = () => (<h1>Hola mundo geek</h1>);
+
+export default About;
